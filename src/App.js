adb93b9f623f18980a904aaff8056b82e16b769d
mardibll <mardimbuulolo@gmail.com>
2023-05-29 05:57:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Belajar membuat repositori ke dalam github
+        </p>
+      <p>Disini saya mencoba deploy project saya, dengan menggunakan verce</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
