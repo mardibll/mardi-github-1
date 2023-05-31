c37c44953dd7b6d3182be0259832fae4c79ab9d9
mardi_buulolo123 <mardimbuulolo@gmail.com>
2023-05-31 13:59:03 +0700

    membuat component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Myclass from "./component/classComponnent";
+import Functions from "./component/functionComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Belajar membuat repositori ke dalam github
-        </p>
-      <p>Disini saya mencoba deploy project saya, dengan menggunakan verce</p>
-      </header>
+      <Myclass text="Saya menggunakan class component" />
+      <Functions text={"Saya menggunakan function component"} />
     </div>
   );
 }
